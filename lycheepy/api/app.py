e556c8acd5dde4d217de16b318d22ad7fc6d3f2f
Gabriel José Bazán <gbazan@outlook.com>
2017-09-30 18:12:31 -0300

    Outputs endpoint.

--- lycheepy/api/app.py
+++ lycheepy/api/app.py
@@ -1,3 +1,6 @@
+import os
+
+from flask import Response, abort
 from flask_cors import CORS
 from simplyrestful.app import app, api
 from simplyrestful.resources import add_resource
@@ -10,6 +13,8 @@ from lycheepy.settings import HOST, PORT, DEBUG
 from lycheepy.wps.service import ServiceBuilder, ProcessesGateway, ChainsGateway
 
 cross_origin = CORS(app, resources={r"*": {"origins": "*"}})
+
+from pywps.configuration import get_config_value
 
 
 add_resource(api, ProcessResource)
@@ -24,6 +29,23 @@ def wps():
     ).extend(
         ChainsGateway.all()
     ).build()
+
+
+@app.route('/wps/outputs/<execution_id>/<filename>')
+def output_file(execution_id, filename):
+    ServiceBuilder().build()
+    outputs_dir = get_config_value('server', 'outputpath')
+    target_file = os.path.join(outputs_dir, execution_id, filename)
+    if os.path.isfile(target_file):
+        file_ext = os.path.splitext(target_file)[1]
+        with open(target_file, mode='rb') as f:
+            file_bytes = f.read()
+        mime_type = None
+        if 'xml' in file_ext:
+            mime_type = 'text/xml'
+        return Response(file_bytes, content_type=mime_type)
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
